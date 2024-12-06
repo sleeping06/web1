3053357c5291f5d22e4396aa1912c9887e6cadfb
sleeping <76526102+sleeping06@users.noreply.github.com>
2024-12-06 20:37:42 +0900

    Add files via upload

---
+++ 4_1_number.py
@@ -0,0 +1,12 @@
+print(1+1)
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print('1-1',1-1)
+print(2/1)
+import math
+print(math.sqrt(4))
+print(math.pow(4,3))
+import random
+print(random.random())
